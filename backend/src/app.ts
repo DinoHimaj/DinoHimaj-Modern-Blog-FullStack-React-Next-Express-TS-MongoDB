08663ea1e0edf30c1c5095e4192ce07f5db3cafa
dinoh <dino.himaj@student.tugraz.at>
2026-05-02 06:13:24 +0200

    fix: add CORS middleware and WEBSITE_URL env variable to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import blogPostsRoutes from './routes/blog-posts';
+import cors from 'cors';
+import env from './env';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: env.WEBSITE_URL,
+    credentials: true,
+  }),
+);
 
 app.use('/posts', blogPostsRoutes);
 
