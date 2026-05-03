f2aba2e791904e0267f20b3cfca067c82c338868
dinoh <dino.himaj@student.tugraz.at>
2026-05-03 04:15:44 +0200

    feat: add request and error logging to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,11 @@ import express from 'express';
 import blogPostsRoutes from './routes/blog-posts';
 import cors from 'cors';
 import env from './env';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 
